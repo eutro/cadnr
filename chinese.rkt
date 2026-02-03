7f7ec8c74e2086fdba45089a8c6a5bb2336c7902
sennenki <zvmsbackend@outlook.com>
2026-02-03 16:16:20 +0800

    bugfix

--- chinese.rkt
+++ chinese.rkt
@@ -125,7 +125,7 @@
       [else
        (define c (string-ref s i))
        (if (equal? c #\-)
-           (cons (substring s 0 (add1 i)) (substring s (add1 i)))
+           (values (substring s 0 (add1 i)) (substring s (add1 i)))
            (loop (add1 i)))])))
 
 (define (string-maybe-trim-question-mark s)
